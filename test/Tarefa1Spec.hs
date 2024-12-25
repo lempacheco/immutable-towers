733e7a0fb3c5f8f470219a189816cb0a69b679b2
leticiampacheco <a112062@alunos.uminho.pt>
2024-12-25 16:25:54 +0000

    Testes tarefa 1

--- test/Tarefa1Spec.hs
+++ test/Tarefa1Spec.hs
@@ -2,22 +2,97 @@
 module Tarefa1Spec (testesTarefa1) where
 
 import Test.HUnit
+import LI12425
+import Tarefa1 
 
 testesTarefa1 :: Test
 testesTarefa1 =
   TestLabel "Testes Tarefa 1" $
     test
-      [ "basic example test" ~: (2 :: Int) ~=? 1 + 1,
-        "another basic example" ~: True ~=? not False
-      ]
+      [ testesFaux, testesPortais, testesInimigos] 
 
-testesTorres :: Test
-testesTorres =
-  TestLabel "Testes Torres" $
-    test
-      [ "validaPosicoesTorres 1" ~: (2 :: Int) ~=? True,
-        "validaPosicoesTorres 2" ~: True ~=? not False
-      ]
+testesFaux :: Test 
+testesFaux = 
+  TestLabel "Testes funções auxiliares" $ 
+   test 
+     [ "eTerra 1" ~: True ~=? eTerra (0.5,0.5) mapa1, 
+       "eTerra 2" ~: False ~=? eTerra (5.5,0.5) mapa1,
+
+       "eRelva 1" ~: True ~=? eRelva (2.5,0.5) mapa1,
+       "eRelva 2" ~: False ~=? eRelva (0.5,0.5) mapa1,
+
+       "eAgua 1" ~: True ~=? eAgua (3.5,0.5) mapa1,
+       "eAgua 2" ~: False ~=? eAgua (0.5,0.5) mapa1 
+
+     ]
+
+testesPortais :: Test 
+testesPortais = 
+  TestLabel "Testes portais" $ 
+   test 
+     [ "Há pelo menos um portal" ~: True ~=? peloMenosUmPortal [portalA, portalB],
+       "Não há portais" ~: False ~=? peloMenosUmPortal [],
+
+       "Estão todos posicionados sobre a terra" ~: True ~=? validaPosicaoPortal [portalA, portalC] mapa1,
+       "Nem todos estão posicionados sobre a terra" ~: False ~=? validaPosicaoPortal [portalA, portalB, portalC] mapa1,
+       "A lista de portais é vazia (sempre True)" ~: True ~=? validaPosicaoPortal [] mapa1,
+
+       "A base não esta sobreposta sobre nenhum portal" ~: True ~=? sobrepostoBasePortal baseA [portalA, portalB, portalC], 
+       "A base está sobreposta com algum portal" ~: False ~=? sobrepostoBasePortal baseB [portalA, portalB, portalC],
+       "Não existe portais, lista vazia (sempre True)" ~: True ~=? sobrepostoBasePortal baseB [],
+
+       "Não estão sobrepostos portais com torres" ~: True ~=? sobrepostoTorrePortal [torre1, torre2] [portalA,portalC],
+       "Estão sobrepostos portais com torres" ~: False ~=? sobrepostoTorrePortal [torre1, torre2, torreA] [portalA, portalC],
+       "Não existe torres, lista vazia (sempre True)" ~: True ~=? sobrepostoTorrePortal [] [portalA, portalC], 
+       "Não existe portais, lista vazia (sempre True)" ~: True ~=? sobrepostoTorrePortal [torre1, torre2, torreA] []
+     ]
+
+testesInimigos :: Test 
+testesInimigos = 
+  TestLabel "Testes inimigos" $ 
+   test 
+     [ "O inimigo é válido" ~: True ~=? inimigosInicio [inimigoA, inimigoC, inimigoE] [portalA, portalC],
+       "O inimigo não é válido, por causa da posiçao" ~: False ~=? inimigosInicio [inimigoA, inimigoB, inimigoC, inimigoE] [portalA, portalC], 
+       "O inimigo não é válido, por causa da vida" ~: False ~=? inimigosInicio [inimigoA, inimigoC, inimigoD, inimigoE] [portalA, portalC], 
+       "O inimigo não é válido por causa da lita de projéteis" ~: False ~=? inimigosInicio [inimigoA, inimigoC, inimigoE, inimigoF] [portalA, portalC], 
+
+       "O inimigo está sobre Terra" ~: True ~=? inimigosTerra [inimigoA, inimigoC, inimigoE, inimigoG] mapa1, 
+       "Nem todos os inimigos estão na terra (contém Relva)"  ~: False ~=? inimigosTerra [inimigoA, inimigoC, inimigoH, inimigoE, inimigoG] mapa1, 
+       "Nem todos os inimigos estão na terra (contém Agua)" ~: False ~=? inimigosTerra [inimigoA, inimigoC, inimigoI, inimigoE, inimigoG] mapa1, 
+       "Lista de inimigos vazia (sempre True)" ~: True ~=? inimigosTerra [] mapa1,
+
+       "Não há sobreposição" ~: True ~=? inimigosTorre [inimigoA, inimigoC, inimigoG] [torre1, torreB],
+       "Há sobreposição" ~: False ~=? inimigosTorre [inimigoA, inimigoC, inimigoG, inimigoH] [torre1, torreB],
+       "Lista de inimigos vazia (sempre True)" ~: True ~=? inimigosTorre [] [torre1, torre2],
+       "Lista de torres vazia (sempre True)" ~: True ~=? inimigosTorre [inimigoA, inimigoG] [],
+
+       "A velocidade dos inimigos é positiva" ~: True ~=? velocidadeInimigos [inimigoA, inimigoG, inimigoE],
+       "Nem todos os inimigos tem velocidade positiva" ~: False ~=? velocidadeInimigos [inimigoA, inimigoG, inimigoC, inimigoE],
+       "A lista de inimigos é vazia (Sempre True)" ~: True ~=? velocidadeInimigos [],
+       
+       "A lista de projéteis está válida" ~: True ~=? normalizaInimigos [Inimigo {projeteisInimigo = [Projetil {tipoProjetil = Resina}, 
+                                                                                                      Projetil {tipoProjetil = Gelo}]},
+                                                                         Inimigo {projeteisInimigo = []}],
+       "Na lista de projeteis ativos existe mais do que um projetil do mesmo tipo" ~: False ~=? normalizaInimigos 
+            [Inimigo {projeteisInimigo = [Projetil {tipoProjetil = Fogo}, 
+                                          Projetil {tipoProjetil = Fogo}, 
+                                         ]}, 
+             Inimigo {projeteisInimigo = [Projetil {tipoProjetil = Resina}, 
+                                          Projetil {tipoProjetil = Gelo}]}],
+       "Na lista de projéteis ativos coexistem fogo e resina" ~: False ~=? normalizaInimigos 
+            [Inimigo {projeteisInimigo = [Projetil {tipoProjetil = Gelo}, 
+                                          Projetil {tipoProjetil = Resina}, 
+                                         ]},
+             Inimigo {projeteisInimigo = [Projetil {tipoProjetil = Fogo}, 
+                                          Projetil {tipoProjetil = Resina}, 
+                                         ]}],
+       "Na lista de projéteis ativos coexistem fogo e gelo" ~: False ~=? normalizaInimigos 
+            [Inimigo {projeteisInimigo = [Projetil {tipoProjetil = Fogo}, 
+                                          Projetil {tipoProjetil = Gelo}, 
+                                         ]}, 
+             Inimigo {projeteisInimigo = [Projetil {tipoProjetil = Resina}, 
+                                          Projetil {tipoProjetil = Gelo}]}]
+     ]
 
 --mapa com caminho até à base
 mapa1 :: Mapa
@@ -77,3 +152,208 @@ torre2 = Torre
 
 torre3 :: Torre
 torre3 = Torre {posicaoTorre = (0.5,2.5)}
+
+-- portal sobre a terra
+portalA :: Portal 
+portalA = Portal 
+  { 
+    posicaoPortal = (0.5, 0.5),
+    ondasPortal = [] 
+  }
+
+-- portal não está sobre a terra 
+portalB :: Portal 
+portalB = Portal 
+  { 
+    posicaoPortal = (0.5, 1.5),
+    ondasPortal = [] 
+  }
+
+-- portal sobre terra 
+portalC :: Portal 
+portalC = Portal 
+  { 
+    posicaoPortal = (5.5, 2.5),
+    ondasPortal = [] 
+  }
+
+-- base não sobreposta com portal 
+baseA :: Base 
+baseA = Base 
+  { 
+    vidaBase = 5,
+    posicaoBase = (5.5, 4.5),
+    creditosBase = 10
+  }
+
+-- base sobreposta com um portal 
+baseB :: Base 
+baseB = Base 
+  {
+    vidaBase = 5,
+    posicaoBase = (5.5, 2.5), 
+     creditosBase = 10
+  }
+-- esta torre esta sobre a terra, e estará sobreposta com um portal 
+torreA :: Torre
+torreA = Torre 
+ {
+    posicaoTorre = (0.5,0.5),
+    danoTorre = 2,
+    alcanceTorre = 5,
+    rajadaTorre = 7,
+    cicloTorre = 1,
+    tempoTorre = 9,
+    projetilTorre = Projetil {tipoProjetil = Gelo, duracaoProjetil = Finita 1}
+ }
+
+torreB :: Torre 
+torreB = Torre 
+ {
+    posicaoTorre = (2.5,0.5),
+    danoTorre = 2,
+    alcanceTorre = 5,
+    rajadaTorre = 7,
+    cicloTorre = 1,
+    tempoTorre = 9,
+    projetilTorre = Projetil {tipoProjetil = Gelo, duracaoProjetil = Finita 1}
+ }
+
+
+-- Este inimigo tem a posiçao inicial de um portal valido. Ou seja, é valido 
+inimigoA :: Inimigo 
+inimigoA = Inimigo 
+ {
+  posicaoInimigo = (0.5,0.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = 10.0,
+  velocidadeInimigo = 10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [] 
+ }
+
+-- Este inimigo não tem a posiçao inicial de um portal valido. Ou seja, é inválido
+inimigoB :: Inimigo 
+inimigoB = Inimigo 
+ {
+  posicaoInimigo = (1.5,1.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = 10.0,
+  velocidadeInimigo = 10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [] 
+ }
+
+-- Este inimigo tem a vida positiva. Ou seja, é valido 
+inimigoC :: Inimigo
+inimigoC = Inimigo 
+ {
+  posicaoInimigo = (0.5,0.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = 10.0,
+  velocidadeInimigo = -10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [] 
+ }
+
+-- Este inimigo não tem a vida positiva. Ou seja, é inválido
+inimigoD :: Inimigo 
+inimigoD = Inimigo 
+ {
+  posicaoInimigo = (0.5,0.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = -2.0,
+  velocidadeInimigo = 10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [] 
+ }
+
+-- Este inimigo tem a lista de projéteis ativos vazia. Ou seja, é valido 
+inimigoE :: Inimigo 
+inimigoE = Inimigo 
+ {
+  posicaoInimigo = (5.5,2.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = 10.0,
+  velocidadeInimigo = 10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [] 
+ }
+
+-- Este inimigo não tem a lista de projéteis ativos vazia. Ou seja, é inválido
+inimigoF :: Inimigo 
+inimigoF = Inimigo 
+ {
+  posicaoInimigo = (0.5,0.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = 10.0,
+  velocidadeInimigo = 10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [projetilA, projetilB] 
+ }
+
+-- Este inimigo é válido, e está sobre a terra.
+inimigoG :: Inimigo 
+inimigoG = Inimigo 
+ {
+  posicaoInimigo = (1.5,3.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = 10.0,
+  velocidadeInimigo = 10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [] 
+ }
+
+-- Esta sobre Relva
+inimigoH :: Inimigo 
+inimigoH = Inimigo 
+ {
+  posicaoInimigo = (2.5,0.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = 10.0,
+  velocidadeInimigo = 10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [] 
+ }
+
+-- Esta sobre Agua
+inimigoI :: Inimigo 
+inimigoI = Inimigo 
+ {
+  posicaoInimigo = (5.5,0.5),
+  direcaoInimigo = Norte,
+  vidaInimigo = 10.0,
+  velocidadeInimigo = 10.0,
+  ataqueInimigo = 5.0, 
+  butimInimigo = 5, 
+  projeteisInimigo = [] 
+ }
+
+projetilA :: Projetil
+projetilA = Projetil
+  { tipoProjetil = Fogo,
+    duracaoProjetil = Finita 5.0 
+  }
+
+projetilB :: Projetil
+projetilB = Projetil
+  { tipoProjetil = Gelo,
+    duracaoProjetil = Infinita 
+  }
+
+projetilC :: Projetil 
+projetilC = Projetil 
+ {tipoProjetil = Resina, 
+  duracaoProjetil = Infinita
+ }
+
+
+
